d22e5f83259ce7b7adacec1cc26c71e4fe4bf6a2
Shehzad <shehzad@spekit.co>
2020-05-04 22:16:21 +0500

    updates the settings file to wire up postgres database

--- realestate/settings.py
+++ realestate/settings.py
@@ -13,8 +13,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+import env
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
@@ -26,7 +27,6 @@ SECRET_KEY = 't!9o(w@a3%y(n#u5ku+@(45fj&caugg5s@o=6d*7#8$g4ipp_q'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -69,17 +69,25 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'realestate.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "spekit.db.backends.postgresql",
+        "NAME": "realestate",
+        "USER": "realestate",
+        "PASSWORD": "realestate",
+        "HOST": "localhost",
+        "PORT": "5432",
+        "TEST": {"NAME": "realestate_test"},
+        "CONN_MAX_AGE": 0 if DEBUG else 3600,
     }
 }
 
+DATABASES = {
+    "default": env.db()  # Raises ImproperlyConfigured exception if DATABASE_URL not in os.environ
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
@@ -99,7 +107,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
@@ -112,7 +119,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
